5649ad68edd9075109d5e44bd0059c4708589f2e
nathan.ave <nathan.ave@epita.fr>
2021-11-22 23:09:28 +0100

    Nathan: Premier push de echo

---
+++ src/builtins/builtins.h
@@ -0,0 +1,22 @@
+#ifndef BUILTINS_H
+#define BUILTINS_H
+
+enum flags_echo
+{
+    NO_OPTIONS = 0,
+    E_OPTIONS,
+    N_OPTIONS,
+    N_E_OPTIONS,
+};
+
+//Pour l'instant echo ne retourne aucune erreurs car tout les cas
+//semble se gérer seul pour le moment. A voir ce brice m'envoie.
+/**
+ * @brief echo builtin with -e and -n options.
+ * 
+ * @param toPrint : message that will be printed.
+ * @param flags : option echo was called with.
+ */
+void echo(char *toPrint, enum flags_echo flags);
+
+#endif
