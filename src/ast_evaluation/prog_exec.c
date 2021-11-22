54747e81d1775da440357d0a3a651d8a3c846108
brice.boisson <brice.boisson@epita.fr>
2021-11-22 23:15:05 +0100

    First version of the function exec, basic structure of the algorithm add.

--- src/ast_evaluation/prog_exec.c
+++ src/ast_evaluation/prog_exec.c
@@ -1,5 +1,38 @@
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
 #include "ast_evaluation_tools.h"
 
 /*
  * The main objectif of this source code is to call some programe of the computer using fork and exec
  */
+
+ int call_exec(char *name)
+ {
+    pid_t cpid = fork();
+    if (cpid == -1)
+    {
+        perror("42sh");
+        return 1;
+    }
+    else if (!cpid)
+    {
+        return execvp(name,NULL);
+    }
+
+    int cstatus = 0;
+    if (waitpid(cpid, &cstatus, 0) == -1)
+    {
+        perror("42sh");
+        return 1;
+    }
+
+    if (!(WIFEEXITED(cstatus) && WEXITSTATUS(cstatus) == 0))
+    {
+        perror("42sh");
+        return 1;
+    }
+
+    return 0;
+ }
