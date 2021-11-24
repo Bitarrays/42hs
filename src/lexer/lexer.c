d139afe44c759ab218efb2b11a459e68f7403a05
charles.simon-meunier <charles.simon-meunier@epita.fr>
2021-11-24 21:22:17 +0100

    lexer: fix ELSE, ELIF

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -202,8 +202,8 @@ static char *get_token_string(enum token_type type)
 {
     char *token_string[] = { "ERROR",
                              "IF",
-                             "ELIF",
                              "ELSE",
+                             "ELIF",
                              "FI",
                              "THEN",
                              "SEMICOLON",
