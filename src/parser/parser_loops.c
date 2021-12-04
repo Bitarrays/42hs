d29570d5f5fd60ad7279d3f951c4a0bd5ac88c95
quentin.fisch <quentin.fisch@epita.fr>
2021-12-04 20:49:37 +0100

    parser: deleted log

--- src/parser/parser_loops.c
+++ src/parser/parser_loops.c
@@ -156,7 +156,6 @@ enum parser_status parse_for(struct ast **ast, struct lexer *lexer)
         while (tok->type == TOKEN_WORD || tok->type == TOKEN_WORD_DOUBLE_QUOTE
                || tok->type == TOKEN_WORD_SINGLE_QUOTE)
         {
-            printf("val: %s\n", tok->value);
             len++;
             values = realloc(values, (len + 1) * sizeof(char *));
             values[len - 1] = tok->value;
