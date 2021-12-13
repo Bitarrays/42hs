1cb2ed3881399263fa945773302da3fe0f09f02b
charles.simon-meunier <charles.simon-meunier@epita.fr>
2021-12-13 12:27:15 +0000

    dot: fix PATH, fix return code

--- src/builtins/dot.c
+++ src/builtins/dot.c
@@ -16,71 +16,70 @@
 
 extern struct shell *shell;
 
-// static char **split_path(char *path)
-// {
-//     char *rest = path;
-//     char *token;
-//     char **paths = NULL;
-//     int paths_nb = 0;
-//     while ((token = strtok_r(rest, ":", &rest)) != NULL)
-//     {
-//         paths = realloc(paths, sizeof(char *) * (paths_nb + 2));
-//         paths[paths_nb++] = token;
-//     }
-//     paths[paths_nb] = NULL;
-//     return paths;
-// }
+static char **split_path(char *path)
+{
+    char *rest = path;
+    char *token;
+    char **paths = NULL;
+    int paths_nb = 0;
+    while ((token = strtok_r(rest, ":", &rest)) != NULL)
+    {
+        paths = realloc(paths, sizeof(char *) * (paths_nb + 2));
+        paths[paths_nb++] = strdup(token);
+    }
+    paths[paths_nb] = NULL;
+    return paths;
+}
 
-// static char *find_in_path(const char *input)
-// {
-//     char *path=getenv("PATH");
-//     if (!path)
-//     {
-//         fprintf(stderr, "42sh: .: %s: not found\n", input);
-//         shell->return_code = 2;
-//         return NULL;
-//     }
-//     char **path_single = split_path(path);
-//     if (!path_single)
-//     {
-//         fprintf(stderr, "42sh: .: %s: not found\n", input);
-//         shell->return_code = 2;
-//         return NULL;
-//     }
-//     int i = 0;
-//     bool found = false;
-//     char *final_path = NULL;
-//     while (path_single[i] && !found)
-//     {
-//         char *full_path = calloc(strlen(path_single[i]) + strlen(input) + 2, sizeof(char));
-//         strcpy(full_path, path_single[i]);
-//         strcat(full_path, "/");
-//         strcat(full_path, input);
-//         if (access(full_path, F_OK) == 0)
-//         {
-//             found = true;
-//             final_path = full_path;
-//             continue;
-//         }
-//         free(full_path);
-//         i++;
-//     }
-//     free(path_single);
-//     struct stat sb;
-//     if (!final_path)
-//     {
-//         fprintf(stderr, "42sh: .: %s: not found\n", input);
-//         shell->return_code = 127;
-//     }
-//     else if (!(stat(final_path, &sb) == 0 && sb.st_mode & S_IXUSR))
-//     {
-//         fprintf(stderr, "42sh: .: %s: Permission denied\n", input);
-//         shell->return_code = 126;
-//         free(final_path);
-//         final_path = NULL;
-//     }
-//     return final_path;
-// }
+static char *find_in_path(const char *input)
+{
+    char *path_tmp=getenv("PATH");
+    char *path = calloc(strlen(path_tmp) + 1, sizeof(char));
+    strcpy(path, path_tmp);
+    if (!path)
+    {
+        fprintf(stderr, "42sh: .: %s: not found\n", input);
+        shell->return_code = 2;
+        free(path);
+        return NULL;
+    }
+    char **path_single = split_path(path);
+    if (!path_single)
+    {
+        fprintf(stderr, "42sh: .: %s: not found\n", input);
+        shell->return_code = 2;
+        free(path);
+        return NULL;
+    }
+    int i = 0;
+    bool found = false;
+    char *final_path = NULL;
+    while (path_single[i] && !found)
+    {
+        char *full_path = calloc(strlen(path_single[i]) + strlen(input) + 2, sizeof(char));
+        strcpy(full_path, path_single[i]);
+        strcat(full_path, "/");
+        strcat(full_path, input);
+        struct stat sb;
+        if (access(full_path, F_OK) == 0 && (stat(path, &sb) == 0 && sb.st_mode & S_IXUSR))
+        {
+            found = true;
+            final_path = full_path;
+            continue;
+        }
+        free(full_path);
+        free(path_single[i]);
+        i++;
+    }
+    if (!final_path)
+    {
+        fprintf(stderr, "42sh: .: %s: not found\n", input);
+        shell->return_code = 2;
+    }
+    free(path);
+    free(path_single);
+    return final_path;
+}
 
 static char get_first_char(const char *arg)
 {
@@ -93,7 +92,7 @@ static char get_first_char(const char *arg)
 char *get_file_content(const char *path)
 {
     if (get_first_char(path) != '/')
-        return NULL;
+        path = find_in_path(path);
     if (!path)
         return NULL;
     char *buffer = 0;
@@ -127,7 +126,7 @@ char *get_file_content(const char *path)
 
 int dot(char **argv)
 {
-    struct var *save = shell->var_list;
+    struct var *save_var_list = shell->var_list;
     shell->var_list = NULL;
     if (!argv[1])
         return 0;
@@ -136,16 +135,8 @@ int dot(char **argv)
         return shell->return_code;
     int res = parse_input(buf);
     free(buf);
-    struct var *tmp = shell->var_list;
-    while (tmp)
-    {
-        struct var *next = tmp->next;
-        free(tmp->name);
-        free(tmp->value);
-        free(tmp);
-        tmp = next;
-    }
-    shell->var_list = save;
+    free_list(shell);
+    shell->var_list = save_var_list;
     return res;
     return shell->return_code;
 }
